e37b6ca15d31963c896e3b2ac357ee6c28611be2
Thomas Ashton <teashton@gmail.com>
2017-08-17 17:45:56 -0600

    magic 8 ball with colors working!!!

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,38 +1,53 @@
 require 'colorize'
 
 Answer = [
-  'It is certain'.colorize(:blue).colorize(:blue),
-  'It is decidedly so'.colorize(:blue),
-  'Without a doubt'.colorize(:blue),
-  'Yes definitely'.colorize(:blue),
-  'You may rely on it'.colorize(:blue),
-  'As I see it, yes'.colorize(:blue),
-  'Most likely'.colorize(:blue),
-  'Outlook good'.colorize(:blue),
-  'Signs point to yes'.colorize(:blue),
-  'Reply hazy try again'.colorize(:gold),
-  'Ask again later'.colorize(:gold),
-  'Better not tell you now'.colorize(:gold),
-  'Cannot predict now'.colorize(:gold),
-  'Concentrate and ask again'.colorize(:gold),
-  'Probably not'.colorize(:red),
-  'My reply is no'.colorize(:red),
-  'My sources say no'.colorize(:red),
-  'Outlook not so good'.colorize(:red),
-  'Very doubtful'.colorize(:red),
+  'It is certain'.colorize(:blue).colorize( :background => :light_green),
+  'It is decidedly so'.colorize(:blue).colorize( :background => :light_green),
+  'Without a doubt'.colorize(:blue).colorize( :background => :light_green),
+  'Yes definitely'.colorize(:blue).colorize( :background => :light_green),
+  'You may rely on it'.colorize(:blue).colorize( :background => :light_green),
+  'As I see it, yes'.colorize(:blue).colorize( :background => :light_green),
+  'Most likely'.colorize(:blue).colorize( :background => :light_green),
+  'Outlook good'.colorize(:blue).colorize( :background => :light_green),
+  'Signs point to yes'.colorize(:blue).colorize( :background => :light_green),
+  'Reply hazy try again'.colorize(:blue).colorize( :background => :light_yellow),
+  'Ask again later'.colorize(:blue).colorize( :background => :light_yellow),
+  'Better not tell you now'.colorize(:blue).colorize( :background => :light_yellow),
+  'Cannot predict now'.colorize(:blue).colorize( :background => :light_yellow),
+  'Concentrate and ask again'.colorize(:blue).colorize( :background => :light_yellow),
+  'Probably not'.colorize(:white ).colorize( :background => :red),
+  'My reply is no'.colorize(:white ).colorize( :background => :red),
+  'My sources say no'.colorize(:white ).colorize( :background => :red),
+  'Outlook not so good'.colorize(:white ).colorize( :background => :red),
+  'Very doubtful'.colorize(:white ).colorize( :background => :red),
 ]
 
 def welcome
-  puts 'Welcome to the Magic 8 Ball!'
-  puts 'Type your question to get an answer, or type q to quit the program!'
-  if gets.downcase.strip == 'q'
-    exit
-  end
-  if gets.strip
-    puts Answer.sample
-  end
+  puts
+  puts '-----------------------------'
+  puts 'Welcome to the Magic 8 Ball!'.colorize(:light_blue )
+  puts '---------------------------------------------------------------------'
+  puts 'Type your question to get an answer, or type q to quit the program!'.colorize(:light_blue )
+  puts '---------------------------------------------------------------------'
+  puts
+  input
 end
+def input
+  users_input = gets.strip
+  puts
+  if users_input.downcase == 'q'
+    exit
+  else
+    puts '-----------------------'
+    puts Answer.sample
+    puts '-----------------------'
+  end
+  restart
+end
+def restart
 while
   true
   welcome
 end
+end
+restart
